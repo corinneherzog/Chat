c30501298f73597452c5c912f695b6ade8afc36f
simonesawyer <simonesawyer@users.noreply.github.com>
2017-05-23 20:41:20 -0700

    Update ClientTest.java

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -5,25 +5,24 @@ public class UserInterface {
    private JFrame frame;
    private JButton sendButton;
    private JButton doneButton;
-   private JTextArea area;
-   public UserInterface{
+   public UserInterface(){
        this.frame = new JFrame();
        this.sendButton = new JButton();
        this.doneButton = new JButton();
    }
-   public void creatingChatFrame{
+   public void creatingChatFrame(){
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setLayout(new FlowLayout());
       frame.setSize(new Dimension(300,175));
       frame.setTitle("Chat Box");
-      this.area = new JTextArea(5,20);
+      JTextArea area = new JTextArea(5,20);
       frame.add(area);
       frame.add(new JScrollPane(area));
       creatingChatButtons();
       frame.setVisible(true);
    }
    
-   public void creatingChatButtons(){
+   private void creatingChatButtons(){
       sendButton.setText("send");
       sendButton.setBackground(Color.WHITE);
       doneButton.setText("done");
