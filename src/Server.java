fe4586f6a8cd2119a62901b9868bd8e570a221d2
simonesawyer <simonesawyer@users.noreply.github.com>
2017-05-24 17:49:10 -0700

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,15 @@
+import com.sun.net.httpserver.HttpServer;
+//use for later
+//import com.sun.net.httpserver.HttpExchange;
+//import com.sun.net.httpserver.HttpHandler;
+import java.net.InetSocketAddress;
+
 public class Server{
-  com.sun.net.httpserver.*;
-  
 
-
+  public static void main(String [] args)throws Exception{
+    HttpServer testServer = HttpServer.create(new InetSocketAddress(8000), 5);
+    testServer.createContext("/hablabamos");
+    //need to create a handler to deal with requests
+    testServer.start();
+  }
 }
