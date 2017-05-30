9961bf352f1802c13ba12c463db62c7f50be2c21
corinneherzog <Corinneh2001@gmail.com>
2017-05-29 20:21:05 -0700

    Update and rename ServerCorinne.java to Server.java
    Added changes to parse jSon and save data in a hashmap

--- src/Server.java
+++ src/Server.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
 
+
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class ServerCorinne {
+import javax.xml.bind.DatatypeConverter;
 
-    static byte [] message = new byte[2000];
+public class Server {
+    static HashMap<String, ArrayList<Message>> hm = new HashMap<>();
+
+    static byte [] messageOut = new byte[2000];
 
     public static void main(String[] args) throws IOException{
         HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
@@ -26,26 +31,42 @@ public class ServerCorinne {
 
     static class Handler implements HttpHandler {
         public void handle(HttpExchange hte) throws IOException{
+        final Gson gson = new Gson();
         InputStream is = hte.getRequestBody();
         byte [] binput = new byte[1000];
         int size = is.read(binput,0,binput.length);
 
         byte [] bresponse;
-        //String txtResponse = "hi there"+binput.toString();
 
-        //bresponse = txtResponse.getBytes();
         String cmd = hte.getRequestMethod();
         if (cmd.equals("POST")) {
-            message = binput;
+            Message currentMessage = storeData(binput , gson);
+            messageOut = gson.toJson(currentMessage).getBytes();
             bresponse = "".getBytes();
+
         } else {
-            bresponse = message;
+            bresponse = messageOut;
         }
         hte.sendResponseHeaders(200,bresponse.length);
         OutputStream os = hte.getResponseBody();
         os.write(bresponse);
         os.close();
         }
+
+        public Message storeData(byte[] binput , Gson gson){
+            String jSon = DatatypeConverter.printBase64Binary(binput);
+            System.out.println(jSon); // for testing
+            Message message = gson.fromJson(jSon, Message.class);
+            message.timeStamp = new Timestamp(System.currentTimeMillis());
+            String reciever = message.reciever;
+            System.out.println(message); //for testing
+            if((!(hm.containsKey(reciever)))){
+                ArrayList<Message> list = new ArrayList<>();
+                hm.put(reciever, list);
+            }
+            hm.get(reciever).add(message);
+            return message;
+        }
     }
-    //HashMap<String, ArrayList<String>> hm = new HashMap<>();
 }
+
