b491df6d6fffa5902d54267041de8e047690a6a4
corinneherzog <Corinneh2001@gmail.com>
2017-06-06 22:48:00 -0700

    Update Server.java
    Fixed bugs + added get functionality

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,6 @@
 /**
  * Created by corinne on 5/24/17.
  */
-/**
- * Created by corinne on 5/24/17.
- */
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.sql.Timestamp;
@@ -15,6 +12,8 @@ import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
+import com.sun.net.httpserver.Headers;
 
 public class Server {
     static HashMap<String, ArrayList<Message>> hm = new HashMap<>();
@@ -22,25 +21,29 @@ public class Server {
 
     public static void main(String[] args) throws IOException{
         HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
-        server.createContext("/testServer" , new Handler());
+        server.createContext("/messages" , new Handler());
         server.setExecutor(null);
         server.start();
     }
 
     static class Handler implements HttpHandler {
         public void handle(HttpExchange hte) throws IOException{
-        final Gson gson = new Gson();
+        Gson gson = new Gson();
         BufferedReader in = new BufferedReader(new InputStreamReader(hte.getRequestBody()));
-        String response;
+        String response = "";
 
         String cmd = hte.getRequestMethod();
+        Headers headers = hte.getRequestHeaders();
+        String user = headers.getFirst("user");
         if (cmd.equals("POST")) {
-            Message currentMessage = storeData(in, gson);
-            messageOut = gson.toJson(currentMessage);
+            storeData(in, gson ,user);
+           // messageOut = gson.toJson(currentMessage);
             response = "";
         }
-        else {
-            response = messageOut;
+        else if(cmd.equals("GET")){
+            ArrayList<Message> list = hm.get(user);
+            response = gson.toJson(list);
+
         }
         hte.sendResponseHeaders(200, response.getBytes().length);
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(hte.getResponseBody()));
@@ -49,17 +52,17 @@ public class Server {
         out.close();
         }
 
-        public Message storeData(BufferedReader in,Gson gson) throws IOException{
-            Message message = gson.fromJson(in, Message.class); //this is where the error is
+        public void storeData(BufferedReader in,Gson gson, String user) throws IOException{
+            String json = in.readLine();
+            Message message = gson.fromJson(json, Message.class); //this is where the error is
             message.timeStamp = new Timestamp(System.currentTimeMillis());
+            message.from = user;
             String receiver = message.to;
             if((!(hm.containsKey(receiver)))) {
                 ArrayList<Message> list = new ArrayList<>();
                 hm.put(receiver, list);
             }
             hm.get(receiver).add(message);
-            return message;
         }
     }
 }
-
